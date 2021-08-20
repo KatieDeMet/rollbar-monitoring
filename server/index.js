e86bbf8e5ec940977a7140bb301c0df1c7a44de6
Katie DeMet <katie.demet7@gmail.com>
2021-08-19 23:29:40 -0500

    get updated

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+app.use(express.json())
 const port = process.env.PORT || 7777
 
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/index.html'))
