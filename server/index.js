06b39d5eb31ec8ca80b090ff30a7651cedbd7290
Katie DeMet <katie.demet7@gmail.com>
2021-08-19 23:17:09 -0500

    get added

--- server/index.js
+++ server/index.js
@@ -6,4 +6,8 @@ const port = process.env.PORT || 7777
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/index.html'))
+})
+
 app.listen(port, () => console.log("Up on lucky 7!"))
