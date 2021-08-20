085536dc20d60fe251913a090c12b82c9ae913be
Katie DeMet <katie.demet7@gmail.com>
2021-08-20 07:57:58 -0500

    changed path

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 7777
 
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/client/index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 app.listen(port, () => console.log("Up on lucky 7!"))
